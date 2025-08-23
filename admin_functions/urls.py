aa19ac74ee4033d5041e466f1391c603c7275d4f
Abishek129 <abishek.reddy.020502@gmail.com>
2025-08-23 15:27:10 +0530

    cashfree1

--- admin_functions/urls.py
+++ admin_functions/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import BlogViewSet, TestimonialViewSet, CoachProfileViewSet, CoachCertificationViewSet, PlansViewSet, location_view, PriceAndPlans, RecommendCoachAPIView,RBuyNowAPIView, RPaymentInitializationView, RPaymentVerificationView
 from rest_framework.routers import DefaultRouter
+from .views import CBuyNowAPIView, CPaymentInitializationView, CPaymentVerificationView
 
 router = DefaultRouter()
 router.register(r"blogs", BlogViewSet, basename="blog")
@@ -15,7 +16,9 @@ urlpatterns = [
     path('recommend/', RecommendCoachAPIView.as_view(), name = "recommend"),
     path('int-buy-now/', RBuyNowAPIView.as_view(), name = "buy-now-int"),
     path('int-payment-init/<int:client_id>/', RPaymentInitializationView.as_view(), name = 'payment-init-int'),
-
-    path('int-payment-verify/', RPaymentVerificationView.as_view(), name = "payment-verify" ),
+    path('int-payment-verify/', RPaymentVerificationView.as_view(), name = "payment-verify-int" ),
+    path('ind-buy-now/', CBuyNowAPIView.as_view(), name = "ind-buy-now" ),
+    path('ind-payment-init/<int:client_id>/', CPaymentInitializationView.as_view(), name = 'payment-init-ind'),
+    path('ind-payment-verify/', CPaymentVerificationView.as_view(), name = "payment-verify-ind")
 
 ] 
