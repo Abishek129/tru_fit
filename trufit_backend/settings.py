17b6fe3bfd7777ac79a95e6fc649bd7b95a0ff68
Abishek129 <abishek.reddy.020502@gmail.com>
2025-12-10 23:20:09 +0530

    added_vercel_betrufit

--- trufit_backend/settings.py
+++ trufit_backend/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
 AUTH_USER_MODEL = 'admin_functions.User'
 CORS_ALLOWED_ORIGINS = [
-    
+    "https://tru-fit.vercel.app/",
     "https://www.betrufit.com",
     "https://betrufit.com",
     "https://dashboard.betrufit.com",
